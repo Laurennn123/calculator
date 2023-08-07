4d10be4ebc0933e3013cd8b3ae4491876951e78f
Laurennn123 <2021-100715@rtu.edu.ph>
2023-08-08 04:36:03 +0800

    updated algorithms in the add and equal buttons

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,111 @@
+var a = 0;
 var storedNumber = [];
+var click = true;
+var newLength;
 var lastLength;
-var notClick = true;
 
-$("#number1").click( () => {
-    parseInt(document.querySelector("#paragraph").innerHTML += $("#number1").text());
-    console.log($("#paragraph").text().length);
-    notClick = true;
-});
+for(let i = 0; i < $(".calculatorButtons").length; i++) {
+    document.querySelectorAll(".calculatorButtons")[i].addEventListener("click", function() {
+        let clickedButton = this.textContent;
+        calculatorButtons(clickedButton);
+    });
+};
 
-$("#plus").click( () => {
+function calculatorButtons(clickedButton) {
+    let operators = ["+", "-", "/", "*", "="];
 
-    if(notClick) {
-        document.querySelector("#paragraph").innerHTML += $("#plus").text();
-        var index = $("#paragraph").text();
-        index = index.slice(0, index.length - 1)
-        lastLength = index;
-        storedNumber.push(parseInt(index));
-        notClick = false;
+    if($("#text-area").text() === clickedButton && clickedButton === "0") {
+        click = false;
+    } else if(operators.includes(clickedButton)) {
+        calculator(clickedButton);
+    } else if($("#text-area").text() != 0) {
+        document.querySelector("#text-area").innerHTML += $("#button" + clickedButton).text();
+        let data = $("#text-area").text();
+        if(newLength > lastLength) {
+            let nextNumber = data.slice(lastLength + 1, $("#text-area").text().length);
+            storedNumber[a] = parseInt(nextNumber);
+        }else {
+            let previousNumber = data.slice(0, $("#text-area").text().length);
+            storedNumber[a] = parseInt(previousNumber);
+        }
+        click = true;
+        console.log($("#text-area").text().length);
+    } else {
+        clickedButton = parseInt(clickedButton);
+        storedNumber[a] = clickedButton;
+        $("#text-area").text(clickedButton);
+        console.log(typeof clickedButton);
     }
+};
+
+function calculator(operator) {
+    switch (operator) {
+        case "+":
+            add(operator);
+            break;
+        case "=":
+            total()
+            break;
+        default:
+            break;
+    }
+}
+
+function add(plusSign) {
+    if(click) {
+        if(storedNumber.length === 0 ){
+            storedNumber[a] = 0;
+        }
+        sequenceData()
+        document.querySelector("#text-area").innerHTML += plusSign;
+        click = false;
+    }
+}
+
+function total() {
+    let previousTotal = [];
+    let total = 0;
+    for(let i = 0; i < storedNumber.length; i++){
+        total += storedNumber[i];
+    }
+    previousTotal = [total];
+    if(click) {
+        $("#text-area").text(total);
+        click = false;
+    } 
+    // else {
+    //     var totalOf = previousTotal[0] += storedNumber[a];
+    //     document.querySelector("#text-area").innerHTML + totalOf.toString();
+    // }
+}
+
+function sequenceData() {
+    a++;
+    lastLength = $("#text-area").text().length;
+    newLength = $("#text-area").text().length + 1;
+}
+
+// $("#plus").click( () => {
+
+//     if(notClick) {
+//         document.querySelector("#paragraph").innerHTML += $("#plus").text();
+//         var index = $("#paragraph").text();
+//         index = index.slice(0, index.length - 1)
+//         lastLength = index;
+//         storedNumber.push(parseInt(index));
+//         notClick = false;
+//     }
      
-})
+// })
 
-$("#equal").click( () => {
+// $("#equal").click( () => {
     
-    if(notClick) {
-        var index = $("#paragraph").text();
-        index = index.slice(lastLength.length, index2.length + 1);
-        storedNumber.push(parseInt(index));
-        console.log(storedNumber[0] + storedNumber[1]);
-        notClick = false;
-    }
+//     if(notClick) {
+//         var index = $("#paragraph").text();
+//         index = index.slice(lastLength.length, index.length + 1);
+//         storedNumber.push(parseInt(index));
+//         $("#paragraph").html(storedNumber[0] + storedNumber[1]);
+//         notClick = false;
+//     }
 
-});
+// });
