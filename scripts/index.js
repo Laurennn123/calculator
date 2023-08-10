2abdf2c510f6818aa811473c55bc158695a618c7
Laurennn123 <2021-100715@rtu.edu.ph>
2023-08-11 03:03:52 +0800

    fixed the algorithm for arithmetic operators

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,28 @@
-var a = 0;
+var index = 0;
 var storedNumber = [];
+var storedOperators = []
 var indexForEqual = [];
-var click = true;
 var newLength;
 var lastLength;
+var click = true;
 
 for(let i = 0; i < $(".calculatorButtons").length; i++) {
     document.querySelectorAll(".calculatorButtons")[i].addEventListener("click", function() {
         let clickedButton = this.textContent;
         calculatorButtons(clickedButton);
+        $("#button" + clickedButton).fadeOut(100).fadeIn(100);
     });
 };
 
 function calculatorButtons(clickedButton) {
-    let operators = ["+", "-", "/", "*", "="];
+    let operators = ['+', '-', '×', '÷', '=', 'CE'];
 
     if(clickedButton === 0) {
         if($("#text-area").text() >= 1) {
-            appear(clickedButton);
+            selectedOperatorAppear(clickedButton);
             let data = $("#text-area").text();
             let previousNumber = data.slice(0, $("#text-area").text().length);
-            storedNumber[a] = parseInt(previousNumber);
+            storedNumber[index] = parseInt(previousNumber);
         } else if(click){
             indexZero(clickedButton);
             click = false;
@@ -28,27 +30,23 @@ function calculatorButtons(clickedButton) {
     } else if(operators.includes(clickedButton)) {
         calculator(clickedButton);
     } else if($("#text-area").text() != 0) {
-        appear(clickedButton);
+        selectedOperatorAppear(clickedButton);
         let data = $("#text-area").text();
 
         if(newLength > lastLength) {
             if(indexForEqual.length === 1) {
-                indexForEqual = [];
-                storedNumber = [];
-                newLength = undefined;
-                lastLength = undefined;
-                a = 0;
+                reset(clickedButton);
                 $("#text-area").text(clickedButton);
                 let previous = $("#text-area").text();
                 let previousNumber = previous.slice(0, $("#text-area").text().length);
-                storedNumber[a] = parseInt(previousNumber);
+                storedNumber[index] = parseInt(previousNumber);
             } else {
                 let nextNumber = data.slice(lastLength + 1, $("#text-area").text().length);
-                storedNumber[a] = parseInt(nextNumber);
+                storedNumber[index] = parseInt(nextNumber);
             }
         } else {
             let previousNumber = data.slice(0, $("#text-area").text().length);
-            storedNumber[a] = parseInt(previousNumber);
+            storedNumber[index] = parseInt(previousNumber);
         }
         click = true;
         console.log($("#text-area").text().length);
@@ -58,21 +56,29 @@ function calculatorButtons(clickedButton) {
     }
 };
 
-function appear(clickedButton) {
-    document.querySelector("#text-area").innerHTML += $("#button" + clickedButton).text();
-}
-
 function indexZero (clickedButton) {
     clickedButton = parseInt(clickedButton);
-    storedNumber[a] = clickedButton;
+    storedNumber[index] = clickedButton;
 }
 
 function calculator(operator) {
     switch (operator) {
-        case "+":
+        case '+':
             add(operator);
             break;
-        case "=":
+        case '-':
+            minus(operator);
+            break;
+        case '×':
+            multiply(operator);
+            break;
+        case '÷':
+            divide(operator);
+            break;
+        case 'CE':
+            reset();
+            break;    
+        case '=':
             total()
             break;
         default:
@@ -80,37 +86,105 @@ function calculator(operator) {
     }
 }
 
-function add(plusSign) {
+function add(plusButton) {
+    operatorOperation(plusButton);
+}
+
+function minus(minusButton) {
+    operatorOperation(minusButton);
+}
+
+function multiply(multiplyButton) {
+    operatorOperation(multiplyButton);
+}
+
+function divide(divideButton) {
+    operatorOperation(divideButton);
+}
+
+function reset() {
+    click = true;
+    if(click) {
+        $("#text-area").text(0);
+    } 
+    indexForEqual = [];
+    storedNumber = [];
+    newLength = undefined;
+    lastLength = undefined;
+    index = 0;
+}
+
+function operatorOperation(operatorButton) {
+    let textArea = $("#text-area").text();
+
     if(click) {
         if($("#text-area").text() === "0"){
-            storedNumber[a] = 0;
+            storedNumber[index] = 0;
         }
         indexForEqual = [];
         sequenceData()
-        document.querySelector("#text-area").innerHTML += plusSign;
+        selectedOperatorAppear(operatorButton);
         click = false;
+    } else {
+        let lastUseOfOperator = textArea.slice(-1);
+        if(lastUseOfOperator != operatorButton ) {
+            $("#text-area").text(textArea.replace(lastUseOfOperator, operatorButton));
+        }
     }
+    storedOperators[index-1] = operatorButton;
+    console.log(storedOperators[index-1])
 }
 
 function total() {
     let total = 0;
-    for(let i = 0; i < storedNumber.length; i++){
-        total += storedNumber[i];
+
+    if(storedOperators.includes('×') && $("text-area").length >= 2){
+        console.log(storedOperators.indexOf('×'));
     }
-    
+
+    for(let i = 0; i < storedNumber.length; i++) {
+        if(i >= 1) {
+            if(storedOperators[i-1] === '+'){
+                total += storedNumber[i];
+            } else if(storedOperators[i-1] === '×'){
+                total *= storedNumber[i];
+            } else if(storedOperators[i-1] === '-'){
+                total -= storedNumber[i];
+            } else if(storedOperators[i-1] === '÷') {
+                total /= storedNumber[i];
+            }
+        } else {
+            total = storedNumber[i];
+        }
+    }
+
     if(click) {
         if(total != $("#text-area").text()) {
-            $("#text-area").text(total);
+            if(total.toString().split('.')[1] >= 1){
+                $("#text-area").text(total.toFixed(2));
+            } else {
+                $("#text-area").text(total);
+            }
             indexForEqual.push("=");
         } else {
-            $("#text-area").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+            animation();
         }
-    } 
+    } else {
+        animation();
+    }
 
 }
 
+function animation() {
+    $("#text-area").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+}
+
+function selectedOperatorAppear(clickedButton) {
+    document.querySelector("#text-area").innerHTML += clickedButton;
+}
+
 function sequenceData() {
-    a++;
+    index++;
     lastLength = $("#text-area").text().length;
     newLength = $("#text-area").text().length + 1;
 }
