c3c17aae3d199267f0f2ba2b1b46dd403b701eac
Laurennn123 <2021-100715@rtu.edu.ph>
2024-07-10 22:31:38 +0800

    fixed removing inside of parens

--- scripts/OtherButtons.js
+++ scripts/OtherButtons.js
@@ -1,11 +1,12 @@
-import { indexForStartingNumber } from "./NumberAndOperatorButtons.js";
-import { storedNumberSelected, storedOperators } from "./NumberAndOperatorButtons.js";
+import { storedNumberSelected, storedOperators, arithmeticOperators } from "./NumberAndOperatorButtons.js";
 import { isTextAreaHaveParenthesis } from "./NumberAndOperatorButtons.js";
 import { calculateTotal } from "./SequenceOfTotal.js";
+import { storedValueForTotal } from "./SequenceOfTotal.js";
+import { mappingStartingIndex } from "./NumberAndOperatorButtons.js";
 
 const otherButtons = ['=', 'CE', '⌫', '.', '(', ')'];
 const [EQUAL, CLEAR_ENTRY, REMOVE, DOT, OPEN_PAREN, CLOSE_PAREN] = otherButtons;
-const closeParenthesisHTML = `<span class="close-parenthesis">)</span>`;
+const closeParenthesisHTML = `<span class="close-parenthesis" id="close-paren">)</span>`;
 let indexOfTotalParenthesis = 0;
 
 const isTextAreaZero = () => {
@@ -20,19 +21,27 @@ const produceOpenOrCloseParenthesis = ({parenthesis, index, isNotNewParenAdded})
         if (isNotNewParenAdded) {
             const getTextAreaWithoutCloseParen = $("#text-area").text().substring(0, $("#text-area").text().length - indexOfTotalParenthesis);
             $("#text-area").text(getTextAreaWithoutCloseParen);
-            document.querySelector("#text-area").innerHTML += `(${closeParenthesisHTML}`;
+            document.querySelector("#text-area").innerHTML += `${OPEN_PAREN}${closeParenthesisHTML}`;
         };
-        document.querySelector("#text-area").innerHTML += parenthesis;
+
+        if (parenthesis === OPEN_PAREN) {
+            document.querySelector("#text-area").innerHTML += parenthesis;
+        } else {
+            document.querySelector("#close-paren").innerText += parenthesis;
+        }
         index--;
         isNotNewParenAdded = false;
     };
 };
 
+let isEqualNotClicked = true;
+
 const otherButton = (buttonClicked) => {
     switch (buttonClicked) {
         case DOT:
             const textArea = $("#text-area").text();
-            const valueInput = textArea.slice(indexForStartingNumber, textArea.length);
+            const index = mappingStartingIndex(textArea);
+            const valueInput = textArea.slice(index, textArea.length);
             
             if (isTextAreaZero()) return $("#text-area").text(DOT);
 
@@ -42,7 +51,60 @@ const otherButton = (buttonClicked) => {
             break;
         case EQUAL:
             const totalValue = calculateTotal(storedNumberSelected, storedOperators);
-            $("#text-area").text(totalValue);
+
+            if (arithmeticOperators.includes($("#text-area").text().slice(-1))) return;
+
+            if (isEqualNotClicked) $("#text-area").text(totalValue), isEqualNotClicked = false;
+            console.log(storedValueForTotal);
+            break;
+        case CLEAR_ENTRY:
+            const clearTextArea = '0';
+            let length = storedNumberSelected.length;
+
+            $("#text-area").text(clearTextArea);
+
+            indexOfTotalParenthesis = 0;
+            isEqualNotClicked = true;
+            while (length !== 0) {
+                storedNumberSelected.pop();
+                storedOperators.pop();
+                storedValueForTotal.pop();
+                length--;
+            };
+            console.log(storedValueForTotal);
+            break;
+        case REMOVE:
+            let indexForRemoving = 1;
+
+            if ($("#text-area").text().includes(OPEN_PAREN)) indexForRemoving = indexOfTotalParenthesis + 1;
+            
+            const removedChar = indexOfTotalParenthesis === 0 ? $("#text-area").text().slice(-1) : $("#text-area").text().slice(-indexForRemoving).slice(0, 1);
+            const previousTextArea = $("#text-area").text().substring(0, $("#text-area").text().length - indexForRemoving);
+            const isLastCharOpenParen = $("#text-area").text().substring(0, $("#text-area").text().length - indexOfTotalParenthesis).slice(-1) === OPEN_PAREN;
+            const isOnlyOneChar = $("#text-area").text().length === 1;
+            const displayNewTextArea = isOnlyOneChar ? '0' : previousTextArea;
+           
+            $("#text-area").text(displayNewTextArea);
+
+            if (isLastCharOpenParen) indexOfTotalParenthesis--;
+            if(previousTextArea.includes(OPEN_PAREN)) {
+                document.querySelector("#text-area").innerHTML += closeParenthesisHTML;
+                if (indexOfTotalParenthesis !== 1 && !isLastCharOpenParen) produceOpenOrCloseParenthesis({parenthesis: CLOSE_PAREN, index: indexOfTotalParenthesis - 1});
+            }
+            
+            const indexStart = mappingStartingIndex($("#text-area").text());
+            const updateValue = $("#text-area").text().slice(indexStart, previousTextArea.length);
+            const isLastCharOperator = arithmeticOperators.includes(removedChar);
+            const lastIndex = isLastCharOperator ? storedOperators.length - 1 : storedNumberSelected.length - 1;
+            if (isLastCharOperator || isLastCharOpenParen) {
+                storedOperators.pop();
+                if (isNaN(storedNumberSelected[storedNumberSelected.length -1])) storedNumberSelected.pop();
+                console.log(storedNumberSelected, storedOperators);
+                return;
+            }
+
+            storedNumberSelected.splice(lastIndex, 1, parseFloat(updateValue));
+            console.log(storedNumberSelected, storedOperators);
             break;
         case OPEN_PAREN:
             const openParenthesis = `(`;
@@ -56,13 +118,13 @@ const otherButton = (buttonClicked) => {
                 $("#text-area").text('');
                 produceOpenOrCloseParenthesis({parenthesis: openParenthesis, index: indexOfTotalParenthesis});
                 document.querySelector("#text-area").innerHTML += openAndCloseParenthesis;
-                produceOpenOrCloseParenthesis({parenthesis: closeParenthesisHTML, index: indexOfTotalParenthesis});
+                produceOpenOrCloseParenthesis({parenthesis: CLOSE_PAREN, index: indexOfTotalParenthesis});
                 return;
             }
 
             if ($("#text-area").text().includes(openParenthesis)) {
                 let notAddedOfNewOpenCloseParen = true;
-                produceOpenOrCloseParenthesis({parenthesis: closeParenthesisHTML, index: indexOfTotalParenthesis, isNotNewParenAdded: notAddedOfNewOpenCloseParen});
+                produceOpenOrCloseParenthesis({parenthesis: CLOSE_PAREN, index: indexOfTotalParenthesis, isNotNewParenAdded: notAddedOfNewOpenCloseParen});
                 indexOfTotalParenthesis++;
                 return;
             };
