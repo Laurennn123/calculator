768201ef7a55b6df1ab4710991ae2b55849527b9
Laurennn123 <2021-100715@rtu.edu.ph>
2024-09-30 14:09:57 +0800

    can work with multiple close paren

--- scripts/SequenceOfTotal.js
+++ scripts/SequenceOfTotal.js
@@ -1,170 +1,272 @@
-const arithmeticOperators = ['+', '−', '×', '÷', '(']; 
-const [ADDITION, SUBTRACTION, MULTIPLICATION, DIVISION, openParenthesis] = arithmeticOperators;
+const OPEN_PAREN = '(';
+const CLOSE_PAREN = ')';
+const arithmeticOperators = ['+', '−', '×', '÷']; 
+const [ADDITION, SUBTRACTION, MULTIPLICATION, DIVISION] = arithmeticOperators;
 
-const isNextOperatorMulDivOrParen = ({operator}) => {
-  return operator === MULTIPLICATION || operator === DIVISION || operator === openParenthesis ? true : false;
-};
-
-const operations = (operator, total, value) => {
-  let equalValue = 0;
-
-  switch (operator) {
-    case MULTIPLICATION:
-      equalValue = total * value;
-      break;
-    case DIVISION:
-      equalValue = value / total;
-      break;
-    case ADDITION:
-      equalValue = total + value;
-      break;
-    case SUBTRACTION:
-      equalValue = value - total;
-      break;  
-    default:
-      break;
-  }
-
-  return equalValue;
-};
-
-const totalValue = (storedValueForTotal, lastTotalValueAtLastParen) => {
-  let total = 0;
-  storedValueForTotal.reverse();
-
-  for (let index = 0; index < storedValueForTotal.length; index++) {
-    for (let indexInOperators = 0; indexInOperators < 1; indexInOperators++) {
-      const value = storedValueForTotal[index][indexInOperators + 1]
-      const operator = storedValueForTotal[index][indexInOperators];
-      const equalValue = index === 0 ? operations(operator, lastTotalValueAtLastParen, value) : operations(operator, total, value);
-      total = equalValue;
+const operations = (operator, num1, num2) => {
+    let newValue;
+    switch (operator) {
+        case '+':
+            newValue = num1 + num2;
+            break;
+        case '−':
+            newValue = num1 - num2;
+            break;
+        case '×':
+            newValue = num1 * num2;
+            break;
+        case '÷':
+            newValue = num1 / num2;
+            break;
     }
-  }
+    return newValue;
+};
 
-  return total;
-}
+const removingElement = (arrIndexRemove, storedNumberSelected, storedOperators) => {
+    for (let i = 0; i < arrIndexRemove.length; i++) {
+        const indexRemove = arrIndexRemove[i];
+        storedNumberSelected.splice(indexRemove, 1);
+        storedOperators.splice(indexRemove, 1);
+        if (arrIndexRemove[i + 1] !== undefined) arrIndexRemove[i + 1] = arrIndexRemove[i + 1] - (i + 1);
+    }
+    return storedNumberSelected, storedOperators;
+};
 
-const indexOfHaveOpBeforeParen = [];
-const storedValueForTotal = [];
+let indexForRemove = [];
 
-const isOperatorBeforeParen = () => {
-  const isOperator = $("#text-area").text().substring(0, indexOfHaveOpBeforeParen[0]).slice(-1);
-  indexOfHaveOpBeforeParen.shift();
-  if (arithmeticOperators.includes(isOperator)) {
-    return true;
-  }
-  return false;
+const multiplyAndDivide = (storedNumberSelected, storedOperators) => {
+    for (let index = 0; index < storedOperators.length; index++) {
+        const currOperator = storedOperators[index];
+
+        if (currOperator === CLOSE_PAREN) {
+            storedOperators.splice(index, 1);
+            continue;   
+        }
+
+        if (currOperator === MULTIPLICATION || currOperator === DIVISION) {
+            const newValue = operations(currOperator, storedNumberSelected[index], storedNumberSelected[index + 1]);
+            indexForRemove.push(index);
+            storedNumberSelected[index + 1] = newValue;
+        } 
+    }
+    return storedNumberSelected, storedOperators;
+};
+
+const plusAndMinus = (storedNumberSelected, storedOperators) => {
+    const isHaveCloseParen = storedOperators.includes(CLOSE_PAREN) ? true : false;
+
+    for (let i = 0; i < storedOperators.length; i++) {
+        const currOperator = storedOperators[i];
+        const nextOperator = storedOperators[i + 1];
+        const prevOperator = storedOperators[i - 1];
+        const operatorNotInclude = [MULTIPLICATION, DIVISION, CLOSE_PAREN];
+
+        if (currOperator === CLOSE_PAREN) {
+            storedOperators.splice(i, 1);
+            continue;
+        }
+        
+        if ((currOperator === ADDITION || currOperator === SUBTRACTION) && nextOperator !== OPEN_PAREN && !operatorNotInclude.includes(prevOperator)) {
+            if (prevOperator === SUBTRACTION && nextOperator === undefined && isHaveCloseParen) {
+                continue;
+            };
+            const newValue = operations(currOperator, storedNumberSelected[i], storedNumberSelected[i + 1]);
+            indexForRemove.push(i);
+            storedNumberSelected[i + 1] = newValue;
+        }
+    }
+    return storedNumberSelected, storedOperators;
 };
 
 const calculateTotal = (storedNumberSelected, storedOperators) => {
-  const split = [...$("#text-area").text()]; 
-  const indexOfParen = split.reduce((accumulator, element, index) => {
-    if (element === openParenthesis && split[index - 1] !== openParenthesis && split[index - 1] !== undefined) accumulator.push(index);
-    return accumulator;
-  }, []);
-  indexOfHaveOpBeforeParen.push(...indexOfParen);
-  const storedMulOrDiv = [];
-  const isStoredOpHaveParens = storedOperators.includes(openParenthesis) ? true : false;
-  let isAddition = false;
+    let indexOpenParen = 0;
+    const splittedText = $("#text-area").text().split('');
 
-  const total = storedNumberSelected.reduce((accumulator, value, index) => {
-    const operator = storedOperators[index - 1];
-    const isNextParenthesis = storedOperators[index] === openParenthesis;
-    const isLastIndex = storedNumberSelected.length - index === 1;
+    const operatorThenParen = splittedText.reduce((indices, element, index) => {
+        if (element === OPEN_PAREN) indexOpenParen++;
+        if (element === OPEN_PAREN && arithmeticOperators.includes(splittedText[index - 1]) && splittedText[index - 1] !== undefined){
+            indices[indexOpenParen] = splittedText[index - 1];
+        }; 
+        return indices;
+    },{});
 
-    if (index === 0 || operator === openParenthesis) {
-      if (isNextParenthesis) {
-        indexOfHaveOpBeforeParen.shift();
-        storedValueForTotal.push([MULTIPLICATION, value]);
-      }
-      if (isLastIndex) {
-        return accumulator = totalValue(storedValueForTotal, value);
-      };
-      return accumulator = value;
-    };
+    indexOpenParen = 0;
+    const indexOfCloseParen = {};
+    const spliceIndex = {};
+    
+    for (let i = 0; i < storedOperators.length; i++) {
+        const currOperator = storedOperators[i];
+        const nextOperator = storedOperators[i + 1];
 
-    if (operator === ADDITION || operator === SUBTRACTION) {
-      isAddition = operator === ADDITION;
+        if (currOperator === OPEN_PAREN) {
+            indexOpenParen++;
+            if (operatorThenParen[indexOpenParen]) storedOperators.splice(i - 1, 1);
+        } else if (currOperator === CLOSE_PAREN) {
+            indexOfCloseParen[indexOpenParen] = nextOperator;
+            const plusNMinus = ['+', '−'];
+            let plusIndex = 1;
+            let j = i;
+            
+            while (storedOperators[j] !== OPEN_PAREN) {
+                if (plusNMinus.includes(storedOperators[j])) plusIndex++;
+                j--;
+            }
 
-      let newTotalBeforeNextValue = operator === ADDITION ? accumulator + value : accumulator - value;
-
-      if (isNextOperatorMulDivOrParen({operator: storedOperators[index]}) && storedOperators[index] !== openParenthesis) {
-        const isOperatorBeforeParen_MulOrDiv = storedOperators[storedOperators.indexOf(openParenthesis, index) - 1] === MULTIPLICATION || storedOperators[storedOperators.indexOf(openParenthesis, index) - 1] === DIVISION;
-        const isAfterNextOperator_MulDivOrParen = storedOperators[index + 1] === MULTIPLICATION || storedOperators[index + 1] === DIVISION || storedOperators[index + 1] === openParenthesis;
-        storedMulOrDiv.push(value);
-        if (isAfterNextOperator_MulDivOrParen && isOperatorBeforeParen_MulOrDiv) {
-          storedValueForTotal.push([operator, accumulator]);
-        };
-        return accumulator;
-      };
-      
-      if (isNextParenthesis) {
-        storedValueForTotal.push([operator, accumulator]);
-        if (isOperatorBeforeParen()) { /* e.g. 6(5-(5)) */
-          if (!isLastIndex) {
-            storedOperators.splice(index, 1);
-            return accumulator = value;
-          };
-          newTotalBeforeNextValue = value;
-        } else {
-          storedValueForTotal.push([MULTIPLICATION, value]);
-        };
-      };
-      
-      if (storedOperators[index + 1] === openParenthesis && !arithmeticOperators.includes(storedOperators[index])) {
-        storedValueForTotal.push([storedOperators[index], newTotalBeforeNextValue]);
-      };
-
-      return accumulator = isLastIndex && storedOperators.includes(openParenthesis) ? totalValue(storedValueForTotal, newTotalBeforeNextValue) : newTotalBeforeNextValue; 
-    };
-
-    if (isNextParenthesis) {
-      if (isOperatorBeforeParen()) {
-        const storeValue = storedMulOrDiv.length === 1 ? storedMulOrDiv[0] : accumulator;
-        storedValueForTotal.push([operator, storeValue]); 
-        if (storedMulOrDiv.length === 1) storedMulOrDiv.pop();
-        if (storedOperators[index + 1] === openParenthesis) {
-          indexOfHaveOpBeforeParen.shift();
-          storedValueForTotal.push([MULTIPLICATION, value]);
+            spliceIndex[indexOpenParen] = plusIndex;
         }
-        storedOperators.splice(index, 1);
-        return accumulator = isLastIndex ? totalValue(storedValueForTotal, value) : value;
-      };
     }
+
+    multiplyAndDivide(storedNumberSelected, storedOperators);
+    removingElement(indexForRemove, storedNumberSelected, storedOperators);
+
+    indexOpenParen = 0;
+    let loopStart = 0;
+
+    for (const index in spliceIndex) {
+        const openParenTarget = parseInt(index);
+        for (let i = loopStart; i < storedOperators.length; i++) {
+            if (storedOperators[i] === OPEN_PAREN) {
+                indexOpenParen++;
+                if (indexOpenParen === openParenTarget) {
+                    const indexAdd = spliceIndex[index];
+                    storedOperators.splice(i + indexAdd, 0, ')');
+                    loopStart = (i + indexAdd) + 1;
+                    break;
+                }
+            }
+        }
+    }   
+
+    indexForRemove = [];
+    plusAndMinus(storedNumberSelected, storedOperators); 
+
+    if (!storedOperators.includes(OPEN_PAREN)) return storedNumberSelected[storedNumberSelected.length - 1];
+
+    removingElement(indexForRemove, storedNumberSelected, storedOperators);
     
-    const newValue = operator === MULTIPLICATION ? storedMulOrDiv[0] * value : storedMulOrDiv[0] / value;
-
-    if (isNextOperatorMulDivOrParen({operator: storedOperators[index]}) && storedMulOrDiv.length === 1) {
-      storedMulOrDiv.splice(0, 1, newValue);
-      if (isNextParenthesis) {
-        storedMulOrDiv.pop();
-        storedValueForTotal.push([MULTIPLICATION, newValue]);
-        return accumulator = 0;
-      };
-      return accumulator;
-    };
+    let start = 0;
+    indexOpenParen = 0;
     
-    if (storedMulOrDiv.length === 1) { /* for next operator either addition or subtraction and stored the value from the previous index */
-      const newTotalBeforeNextValue = isAddition ? accumulator + newValue : accumulator - newValue;
-      storedMulOrDiv.pop();
-      return accumulator = isLastIndex && storedOperators.includes(openParenthesis) ? totalValue(storedValueForTotal, newTotalBeforeNextValue) : newTotalBeforeNextValue;
-    };
+    for (const index in indexOfCloseParen) {
+        const targetNum = parseInt(index);
+        for (let i = start; i < storedOperators.length; i++) {
+            const currOperator = storedOperators[i];
+            if (currOperator === OPEN_PAREN) {
+                indexOpenParen++;
+                if (indexOpenParen === targetNum) {
+                    if (operatorThenParen[indexOpenParen] !== indexOfCloseParen[index] && operatorThenParen[indexOpenParen] !== undefined) {
+                        storedOperators[i] = operatorThenParen[indexOpenParen];
+                        delete operatorThenParen[indexOpenParen];
+                    };
 
-    if (isNextParenthesis) {
-      const valueForSequence = operator === MULTIPLICATION ?  accumulator * value : accumulator / value;
-      storedValueForTotal.push([MULTIPLICATION, valueForSequence]);
-    };
+                    const plusNMinus = ['+', '−'];
+                    let frontOperator = storedOperators[i]; 
+                  
+                    if (frontOperator === OPEN_PAREN) frontOperator = '×';
+                    
+                    if (!plusNMinus.includes(frontOperator)) {
+                        const currValue = storedNumberSelected[i];
+                        const nextValue = storedNumberSelected[i + 1];
+                        
+                        storedOperators.splice(i , 1);
+                        storedNumberSelected[i + 1] = operations(frontOperator, currValue, nextValue);
+                        storedNumberSelected.splice(i, 1);
+                    }
 
-    if (isLastIndex && isStoredOpHaveParens) {
-      const lastValue = operations(operator, value, accumulator);
-      const lastTotalValue = totalValue(storedValueForTotal, lastValue);
-      return accumulator = lastTotalValue;
-    };
+                    start = i;
+                    break;
+                }
+            }
+        }
+    }
 
-    return accumulator = operator === MULTIPLICATION ?  accumulator * value : accumulator / value;
-  }, 0);
+    let isOpenParenReduce = false;
 
-  return total;
+    for (const index in indexOfCloseParen) {
+        const targetNum = isOpenParenReduce ? parseInt(index) - 1 : parseInt(index);
+        isOpenParenReduce = true;
+        for (const indexNum in operatorThenParen) {
+            const operator = operatorThenParen[indexNum];
+            const newIndex = parseInt(indexNum) - 1;
+            if (targetNum <= parseInt(indexNum)) {
+                operatorThenParen[newIndex] = operator;
+                delete operatorThenParen[indexNum];
+            }
+        }
+    }
+
+    indexForRemove = [];
+    indexOpenParen = 0;
+    let indexStart = 0;
+    const indexToInvert = {};
+
+    for (const index in operatorThenParen) {
+        const parenIndex = parseInt(index);
+        const plusNMinus = ['+', '−'];
+        for (let i = indexStart; i < storedOperators.length; i++) {
+            if (storedOperators[i] === OPEN_PAREN) {
+                indexOpenParen++;
+                if (parenIndex === indexOpenParen) {
+                    const prevOperator = storedOperators[i - 1];
+                    if (plusNMinus.includes(prevOperator) && plusNMinus.includes(operatorThenParen[index])) {
+                        storedOperators[i] = operatorThenParen[index];
+                        indexToInvert[i] = '(';
+                        indexStart = i + 1;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    multiplyAndDivide(storedNumberSelected, storedOperators);
+    removingElement(indexForRemove, storedNumberSelected, storedOperators);
+
+    indexForRemove = [];
+
+    for (let i = 0; i < storedOperators.length; i++) {
+        let currOperator = storedOperators[i];
+        const nextOperator = storedOperators[i + 1];
+        const prevOperator = storedOperators[i - 1];
+        const operatorNotInclude = [MULTIPLICATION, DIVISION, CLOSE_PAREN];
+        if (indexToInvert[i]) {
+            currOperator = '(';
+            storedOperators[i] = '(';
+            delete indexToInvert[i];
+        }
+        if ((currOperator === ADDITION || currOperator === SUBTRACTION) && nextOperator !== OPEN_PAREN && !operatorNotInclude.includes(prevOperator)) {
+            const newValue = operations(currOperator, storedNumberSelected[i], storedNumberSelected[i + 1]);
+            indexForRemove.push(i);
+            storedNumberSelected[i + 1] = newValue;
+        }
+    }
+    removingElement(indexForRemove, storedNumberSelected, storedOperators);
+    
+    start = 0;
+    indexOpenParen = 0;
+
+    for (const index in operatorThenParen) {
+        for (let i = start; i < storedOperators.length; i++) {
+            if (storedOperators[i] === OPEN_PAREN) {
+                indexOpenParen++;
+                if (parseInt(index) === indexOpenParen) {
+                    storedOperators[i] = operatorThenParen[index];
+                    start = i;
+                    break;
+                }
+            }
+        }
+    }
+
+    for (let i = storedOperators.length; i > 0; i--) {
+        const currOperator = storedOperators[i - 1];
+        let newValue = operations(currOperator, storedNumberSelected[i - 1], storedNumberSelected[i]);
+
+        if (currOperator === OPEN_PAREN) newValue = operations(MULTIPLICATION, storedNumberSelected[i - 1], storedNumberSelected[i]);
+        storedNumberSelected[i - 1] = newValue;
+    }
+
+    return storedNumberSelected[0];
 };
 
-export { calculateTotal, storedValueForTotal };
+export { calculateTotal };
